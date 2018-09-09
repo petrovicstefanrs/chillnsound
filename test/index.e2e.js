4eb6f4a78f371a5608d4af0f09f5c61b08c26b06
Amila Welihinda <amilajack@gmail.com>
2018-09-08 17:31:31 -0700

    github pages support

--- test/index.e2e.js
+++ test/index.e2e.js
@@ -1,7 +1,7 @@
 import testcafe from 'testcafe';
 
 fixture`Home Page`
-  .page`../src/index.html`;
+  .page`../index.html`;
 
 test('should load the home page', async (t) => {
   await t.click('.play');
